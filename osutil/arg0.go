213e963dfe5e67e4d6f4b28cde752f7ac6c18356
Lonnie Liu <liulonnie@gmail.com>
2021-08-17 13:32:33 -0700

    add arg0base

--- osutil/arg0.go
+++ osutil/arg0.go
@@ -17,12 +17,19 @@ package osutil
 
 import (
 	"os"
+	"path/filepath"
 )
 
-// Arg0 returns the first arg, often represents the name of the binary.
+// Arg0 returns the first arg, often represents the path of the binary.
 func Arg0() string {
 	if len(os.Args) == 0 {
 		return ""
 	}
 	return os.Args[0]
+}
+
+// Arg0Base returns the base name of the first arg, which often represents the
+// name of the binary.
+func Arg0Base() string {
+	return filepath.Base(Arg0())
 }
