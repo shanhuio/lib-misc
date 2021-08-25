fea3a29549117b5144c58a5522d19112ad1e767b
Lonnie Liu <liulonnie@gmail.com>
2021-08-25 10:40:26 -0700

    add a duration struct in timeutil

--- timeutil/duration.go
+++ timeutil/duration.go
@@ -19,28 +19,35 @@ import (
 	"time"
 )
 
-// Timestamp is a struct to record a UTC timestamp.
+func secNano(nano int64) (int64, int64) {
+	sec := nano / 1e9
+	nano -= sec * 1e9
+	if nano < 0 {
+		nano += 1e9
+		sec--
+	}
+	return sec, nano
+}
+
+// Duration is a struct to record a duration.
 // It is designed to be directly usable in Javascript.
-type Timestamp struct {
+type Duration struct {
 	Sec  int64
 	Nano int64 `json:",omitempty"`
 }
 
-// Time returns the time of this timestamp in UTC.
-func (t *Timestamp) Time() time.Time {
-	return time.Unix(t.Sec, t.Nano).UTC()
-}
-
-// NewTimestamp creates a new timestamp from the given time.
-func NewTimestamp(t time.Time) *Timestamp {
-	sec, nano := secNano(t.UnixNano())
-	return &Timestamp{
+// NewDuration creates a new duration of d.
+func NewDuration(d time.Duration) *Duration {
+	nano, sec := secNano(d.Nanoseconds())
+	return &Duration{
 		Sec:  sec,
 		Nano: nano,
 	}
 }
 
-// TimestampNow creates a time stamp of the time now.
-func TimestampNow() *Timestamp {
-	return NewTimestamp(time.Now())
+// Duration converts to time.Duration type.
+func (d *Duration) Duration() time.Duration {
+	sec := time.Duration(d.Sec) * time.Second
+	nano := time.Duration(d.Nano) * time.Nanosecond
+	return sec + nano
 }
